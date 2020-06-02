7215387778d6d9d0b9a18dcd3801bd9b6125b822
Erich Richter <erich.richter@gmail.com>
2020-06-02 16:04:26 -0700

    add mad libs section > > co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,48 @@ $(document).ready(function(){
         $("ul#user").prepend("<li>Hello!</li>");
         $("ul#webpage").prepend("<li>Why hello there!</li>");
         $("ul#user").children("li").first().click(function() {
-            alert('hi')
+            $(this).remove()
       });
     });
       $("button#goodbye").click(function() {
         $("ul#user").prepend("<li>Goodbye!</li>");
         $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
         $("ul#webpage").children("li").first().click(function() {
-            alert('hi');
+            $(this).remove()
       });
     });
       $("button#stop").click(function() {
         $("ul#user").prepend("<li>Stop copying me!</li>");
         $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>")
       });
-});
+
+      $("#formOne").submit(function(event) {
+        const person1Input = $("input#person1").val();
+        const person2Input = $("input#person2").val();
+        const animalInput= $("input#animal").val();
+        const exclamationInput = $("input#exclamation").val();
+        const verbInput = $("input#verb").val();
+        const nounInput = $("input#noun").val();
+    
+        $(".person1").text(person1Input);
+        $(".person2").text(person2Input);
+        $(".animal").text(animalInput);
+        $(".exclamation").text(exclamationInput);
+        $(".verb").text(verbInput);
+        $(".noun").text(nounInput);
+    
+        $("#story").show();
+      
+    
+        event.preventDefault();
+    
+    });
+
+
+
+
+
+
+    });
+
+
